e7b4ed7a54ad1b5bf653f6a48fec3977dd72ccd8
りんご飴196 <kazumac41@gmail.com>
2024-08-20 02:02:17 +0900

    [追加] dbファイル

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/DataBaseManager.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/DataBaseManager.kt
@@ -1,3 +1,35 @@
 package com.github.ringoame196_s_mcPlugin
 
-class DataBaseManager
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+import java.sql.Statement
+
+class DataBaseManager() {
+    fun runSQLCommand(dbFilePath: String, command: String): Any? {
+        var value: Any? = null
+        var statement: Statement? = null
+        try {
+            statement = connection(dbFilePath) // 接続
+            value = statement?.executeUpdate(command)
+        } catch (e: SQLException) {
+            // エラーハンドリング
+            println("SQL Error: ${e.message}")
+        } finally {
+            disconnect(statement?.connection) // 切断
+        }
+        return value
+    }
+
+    private fun connection(dbFilePath: String): Statement? {
+        val connection = DriverManager.getConnection("jdbc:sqlite:$dbFilePath")
+        // SQLステートメントの作成
+        val statement = connection.createStatement()
+        statement.queryTimeout = 30 // タイムアウトの設定
+        return statement
+    }
+
+    private fun disconnect(connection: Connection?) {
+        connection?.close()
+    }
+}
